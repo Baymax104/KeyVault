5b76e228215e783e370c401bc54e67f4c7e46593
Baymax104 <wzy1048168235@163.com>
2024-09-19 21:35:49 +0800

    feat(main): 延长SplashScreen持续时间；优化图标

--- app/src/main/java/top/baymaxam/keyvault/MainActivity.kt
+++ app/src/main/java/top/baymaxam/keyvault/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import top.baymaxam.keyvault.ui.screen.MainScreen
 import top.baymaxam.keyvault.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashscreen = installSplashScreen()
+        setupSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -22,5 +25,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+fun ComponentActivity.setupSplashScreen() {
+    var keep = true
+    installSplashScreen().setKeepOnScreenCondition { keep }
+    lifecycleScope.launch {
+        delay(600)
+        keep = false
     }
 }
